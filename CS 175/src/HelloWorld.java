e8b5e12ef7ca37c69f123593ee7957b889b3077e
Estaniablanc <71726093+Estaniablanc@users.noreply.github.com>
2021-01-28 12:27:49 -0500

    new version

---
+++ CS 175/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World");
+		System.out.println("My partner is joe");
+        System.out.println("Attended Cs-104 in the Fall");
+        System.out.println("Joe Knows Python");
+}
+}
